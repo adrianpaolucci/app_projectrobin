aa8c7e2778f365fd647d8eee2cbd53d5b3bc7904
Adrian <apao0001@student.monash.edu>
2021-01-06 11:23:42 +1100

    -update bug from final display back to intervention main screen
    Signed-off-by: Adrian <apao0001@student.monash.edu>

--- lib/interventionMainScreen.dart
+++ lib/interventionMainScreen.dart
@@ -452,6 +452,7 @@ class _DosingMainState extends State<InterventionMain> {
                               ),
                             ),
                               onTap: () {
+                                previousPage = InterventionMain();
                                 if (navBarIndex != 2) {
                                   Navigator.push(
                                       context,
